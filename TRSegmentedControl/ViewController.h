129afac0f9f35cd67b56bc619554d6a9e2cb94d0
张元武 <674707239@qq.com>
2017-12-22 15:08:48 +0800

    Initial Commit

---
+++ TRSegmentedControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TRSegmentedControl
+//
+//  Created by ZYW on 2017/12/22.
+//  Copyright © 2017年 ZYW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TRSegmentedControl/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TRSegmentedControl
+//
+//  Created by ZYW on 2017/12/22.
+//  Copyright © 2017年 ZYW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
+}
+
+
+@end
